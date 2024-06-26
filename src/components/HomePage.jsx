467e4f9bc7c0e5da12a2f18a18766e678026c86b
Avinash <ak7797405@gmail.com>
2024-06-26 17:50:11 +0530

    style fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
-import './HomePage.scss'
+import './HomePage.scss';
 import Location from "./Location";
 import MapComponent from "./MapComponent";
 import axios from "axios";
 
-
 const HomePage = () => {
   const [route, setRoute] = useState(null);
-  const [distance,setDistance] = useState(0);
-  const [time,setTime] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const [time, setTime] = useState(0);
 
   const handleGenerateRoute = async (locations) => {
     const waypoints = locations.map((loc) => `${loc.value.properties.lat},${loc.value.properties.lon}`).join("|");
-    const apiKey    = process.env.REACT_APP_GEOAPIFY_API_KEY;
-    const apiUrl    = `https://api.geoapify.com/v1/routing?waypoints=${waypoints}&mode=drive&apiKey=${apiKey}`;
+    const apiKey = process.env.REACT_APP_GEOAPIFY_API_KEY;
+    const apiUrl = `https://api.geoapify.com/v1/routing?waypoints=${waypoints}&mode=drive&apiKey=${apiKey}`;
 
     try {
-      const response    = await axios.get(apiUrl);
+      const response = await axios.get(apiUrl);
       console.log(response);
       const routeResult = response.data;
       console.log("Route Result:", routeResult);
@@ -27,24 +26,25 @@ const HomePage = () => {
   };
 
   return (
-  <div className="homePage">
-  <p>Let's calculate <b>distance</b> from Google maps </p>
-  <div className="container">
-    <div className="locationContainer">
-      <Location
-        onGenerateRoute={handleGenerateRoute}
-        time={time}
-        distance={distance}
-      />
+    <div className="homePage">
+      <p>Let's calculate <b>distance</b> from Google maps </p>
+      <div className="container">
+        <div className="locationContainer">
+          <Location
+            onGenerateRoute={handleGenerateRoute}
+            time={time}
+            distance={distance}
+          />
+        </div>
+        <div className="mapContainer">
+          <MapComponent 
+            route={route}
+            setDistance={setDistance}
+            setTime={setTime} 
+          />
+        </div>
+      </div>
     </div>
-    <div className="mapContainer">
-      <MapComponent route={route}
-      setDistance={setDistance}
-      setTime = {setTime} />
-    </div>
-  </div>
-</div>
-
   );
 };
 
